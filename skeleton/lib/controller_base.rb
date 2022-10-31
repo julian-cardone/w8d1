bf6741bc14e144b786bfafbb00960a56d5652961
Julian Cardone <juliancardone1@gmail.com>
2022-10-31 16:46:47 -0400

    finished 02

--- skeleton/lib/controller_base.rb
+++ skeleton/lib/controller_base.rb
@@ -2,26 +2,45 @@ require 'active_support'
 require 'active_support/core_ext'
 require 'erb'
 require_relative './session'
+require 'byebug'
 
 class ControllerBase
   attr_reader :req, :res, :params
 
   # Setup the controller
   def initialize(req, res)
+    @req = req
+    @res = res
   end
 
   # Helper method to alias @already_built_response
   def already_built_response?
+    @already_built_response == @res
   end
 
   # Set the response status code and header
   def redirect_to(url)
+    if !already_built_response?
+      @res.status = 302
+      @res["location"] = url
+      @already_built_response = @res
+      nil
+    else
+      raise ArguementError.new
+    end
   end
 
   # Populate the response with content.
   # Set the response's content type to the given type.
   # Raise an error if the developer tries to double render.
   def render_content(content, content_type)
+    if !already_built_response?
+      @res.write(content)
+      @res["Content-Type"] = content_type
+      @already_built_response = @res
+    else
+      raise ArguementError.new
+    end
   end
 
   # use ERB and binding to evaluate templates
